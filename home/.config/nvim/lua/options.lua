388b78c96c7060104c6cbd31e2751cce16e4830c
wise200 <jrwise200@gmail.com>
2023-03-12 01:32:46 -0500

    Worked on init.lua

---
+++ home/.config/nvim/lua/options.lua
@@ -0,0 +1,12 @@
+vim.opt.number = true -- diplay line numbers
+vim.opt.autoindent = true -- copy indent form line to line
+vim.opt.mouse = 'a' -- gui mouse
+vim.opt.scrolloff = 99 -- keep cursor vertically centered
+vim.opt.tabpagemax = 32 -- allow more tabs open at once TODO: change this
+vim.opt.wrap = false -- don't wrap overflowing lines
+vim.termguicolors = true
+vim.colorcolumn = 80
+vim.ignorecase = true
+vim.smartcase = true
+
+vim.cmd('colorscheme onedark')
